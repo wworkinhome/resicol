fffd1f896752b18b7086a04e08e4f4f57e720b7c
Wilmer Antonio Mosquera Figueroa <wworkinghome@gmail.com>
2023-04-20 22:42:06 -0500

    build: Creaciónn del proyecto que empezaremos a trabajar de forma colaborativa

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'my-custom-color': '#fffff'
+      },
+      screens: {
+        xs: '375px',
+        sm: '600px',
+        md: '900px',
+        lg: '1200px',
+        xl: '1536px',
       },
     },
   },
   plugins: [],
-}
+};
